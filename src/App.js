91daf5b9da55ea43ebd0cfaaafd21782be86667b
guerrillero <asensio.callol@gmail.com>
2020-06-26 13:44:07 -0400

    handleOnSelect function Control initially selected option from parent. Parent component needs to know what option is selected and be able to subscribe to change. Show selected option title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Dropdown from "./Dropdown/Dropdown";
 
 export default function App() {
+  const items = [
+    { id: "1", value: "val 1" },
+    { id: "2", value: "val 2" },
+    { id: "3", value: "val 3" }
+  ];
+  const [optSelected, setOptSelected] = useState({});
+
+  const handleOnSelect = event => {
+    setOptSelected(event);
+  };
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Dropdown />
+      <div className="flex">
+        <div>
+          <Dropdown
+            options={items}
+            placeholder="Select options"
+            selected={optSelected}
+            handleOnSelect={handleOnSelect}
+          />
+        </div>
+        <div>
+          <h3>{`You selected: ${
+            optSelected.value ? optSelected.value : "-"
+          }`}</h3>
+        </div>
+      </div>
     </div>
   );
 }
